65099a657164df8a32bcac4559c7b49f6b5070e9
SYZ1234 <65107905+SYZ1234@users.noreply.github.com>
2020-05-23 10:44:03 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#function1: makeCacheMatrix
+makeCacheMatrix <- function(x= matrix()) {#
+        inv <- NULL
+        set <- function(m){
+                x <<- m
+                inv <<- NULL
+        }
+        get <- function() x
+        setInv <- function(solveMatrix) inv <<- solveMatrix
+        getInv <- function() inv
+        list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
+
+#An example to test the function 'makeCacheMatrix'
+z=makeCacheMatrix(matrix(c(11,12,13,14), nrow = 2, ncol = 2));
+z$get()
+
 
 
 ## Write a short comment describing this function
-
+#function2: cacheSolve
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInv()
+        if(!is.null(inv)){
+                message("Cached data here!")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setInv(inv)
+        inv      
 }
+
+#An example to test the function cacheSolve
+cacheSolve(z)
